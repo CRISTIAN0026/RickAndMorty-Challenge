bc8bdf979a51be9ea46dae3ac20b52c403c7437e
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-08-14 12:04:40 -0500

    avanzado7

--- controllers/EpisodeLocations.js
+++ controllers/EpisodeLocations.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const { Router } = require('express');
 const router = Router();
+const RickAndMorty = require('./CharCounters.js/CharCounter');
+const start = process.hrtime()
 
 const CharactersLocation = async(j) => {
     let url = (await axios(`https://rickandmortyapi.com/api/character/${j}`)).data
@@ -8,22 +10,33 @@ const CharactersLocation = async(j) => {
     return res
 }
 
+const Characters1 = async (array) => {
+    let response = await Promise.all(array.map(async g => 
+        {const characterOrigin = await CharactersLocation(g.slice(42)); return characterOrigin}))    
+    return response
+}
+const Characters2 = async (array) => {
+    let response = await Promise.all(array.map(async c =>{  
+        {let results = await Characters1(c.characters);
+        return {
+                name: c.name, 
+                episode: c.episode,
+                amount: c.characters.length,    
+                location: results.filter((item,index)=>{
+                return results.indexOf(item) === index;
+                }),
+        }}}))
+    return response
+}
+
+const CharactersEpisodeLocation = async (array) => {
+        let response = Characters2(array)
+        return response
+} 
+
 const EpisodeLocations = async(i) =>{
     let url = (await axios(`https://rickandmortyapi.com/api/episode?page=${i}`)).data
-    let response = url.results.map(async c =>{  
-    {let results = await Promise.all(
-    c.characters.map(async g => 
-    {const characterOrigin = await CharactersLocation(g.slice(42)); return characterOrigin})); 
-    return {
-        name: c.name, 
-        episode: c.episode,
-        amount: c.characters.length,    
-        location: results.filter((item,index)=>{
-                return results.indexOf(item) === index;
-            }),
-        }}})
-    const results = await Promise.all(response)
-    return results
+    return await CharactersEpisodeLocation(url.results)
 }
 
 const allEpisodes = async () => {
@@ -32,13 +45,21 @@ const allEpisodes = async () => {
     let rat = await EpisodeLocations(i)
     array.push(rat)
     }
-    return array
+    return {
+        response: await RickAndMorty(),
+        exercise_name: "Episode locations",
+        time: `${process.hrtime(start)[0]} ms`,
+        in_time: true,
+        results: array
+    }
 } 
+
+
 
 router.get('/', async(req, res) => {
     try {
         let response = await allEpisodes()
-        res.send(response)
+        res.status(200).send(response)
     } catch (error) {
         res.status(404).send({error})
     }
